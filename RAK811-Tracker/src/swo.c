bc8610cfc26bc64b233aefacaff0239d5f423ce0
Olof Astrand <olof.astrand@gmail.com>
2019-05-20 08:48:10 +0200

    Improved SWO code

--- RAK811-Tracker/src/swo.c
+++ RAK811-Tracker/src/swo.c
@@ -13,6 +13,12 @@ void SWO_Init(uint32_t portBits, uint32_t cpuCoreFreqHz) {
   uint32_t SWOSpeed = 115200; /* default 115200 baud rate */
   uint32_t SWOPrescaler = (cpuCoreFreqHz / SWOSpeed) - 1; /* SWOSpeed in Hz, note that cpuCoreFreqHz is expected to be match the CPU core clock */
  
+
+    // Disable Jtag
+    //RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+    //AFIO->MAPR |= (2 << 24); // Disable JTAG to release TRACESWO
+    DBGMCU->CR |= DBGMCU_CR_TRACE_IOEN; // Enable IO trace pins
+
     if (SystemCoreClock!=cpuCoreFreqHz) {
         printf("swo clock missmatch %d %d\n",SystemCoreClock,cpuCoreFreqHz);
     }
@@ -31,7 +37,7 @@ void SWO_Init(uint32_t portBits, uint32_t cpuCoreFreqHz) {
   *((volatile unsigned *)(ITM_BASE + 0x40304)) = 0x00000100; /* Formatter and Flush Control Register */
 }
 
-#define portNo 1
+#define portNo 0
 /**
  * SWO_PrintChar()
  *
@@ -48,27 +54,31 @@ void SWO_PrintChar(char c) {
 #if(1)
 //Use CMSIS_core_DebugFunctions. See core_cm3.h
 
-volatile int timeout;
- 
-  /* Check if Trace Control Register (ITM->TCR at 0xE0000E80) is set */
-  if ((ITM->TCR&ITM_TCR_ITMENA_Msk) == 0) { /* check Trace Control Register if ITM trace is enabled*/
-    return; /* not enabled? */
-  }
-  /* Check if the requested channel stimulus port (ITM->TER at 0xE0000E00) is enabled */
-  if ((ITM->TER & (1ul<<portNo))==0) { /* check Trace Enable Register if requested port is enabled */
-    return; /* requested port not enabled? */
-  }
-  timeout = 5000; /* arbitrary timeout value */
-  while (ITM->PORT[0].u32 == 0) {
-    /* Wait until STIMx is ready, then send data */
-    timeout--;
-    if (timeout==0) {
-      return; /* not able to send */
+ // Only ouput to debug registers if debug enabled
+ if ((CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk)==CoreDebug_DHCSR_C_DEBUGEN_Msk) {
+
+    printf("X\n");
+    volatile int timeout;
+    
+    /* Check if Trace Control Register (ITM->TCR at 0xE0000E80) is set */
+    if ((ITM->TCR&ITM_TCR_ITMENA_Msk) == 0) { /* check Trace Control Register if ITM trace is enabled*/
+        return; /* not enabled? */
     }
+    /* Check if the requested channel stimulus port (ITM->TER at 0xE0000E00) is enabled */
+    if ((ITM->TER & (1ul<<portNo))==0) { /* check Trace Enable Register if requested port is enabled */
+        return; /* requested port not enabled? */
+    }
+    timeout = 5000; /* arbitrary timeout value */
+    while (ITM->PORT[0].u32 == 0) {
+        /* Wait until STIMx is ready, then send data */
+        timeout--;
+        if (timeout==0) {
+        return; /* not able to send */
+        }
+    }
+    ITM->PORT[0].u16 = 0x08 | (c<<8);
+
   }
-  ITM->PORT[0].u16 = 0x08 | (c<<8);
-
-
   //ITM_SendChar (c);
  
 #else
